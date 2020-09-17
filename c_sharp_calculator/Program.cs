e2e3b5f66beb9309066862c5cb8011ba2a5d468e
james sewell <jamesjosephsewell@gmail.com>
2020-09-17 03:28:31 -0500

    initial commit

---
+++ c_sharp_calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
